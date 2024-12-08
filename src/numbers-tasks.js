804c6c22edf8955ac12ef845edbf528c069607a2
alyona317 <118733159+alyona317@users.noreply.github.com>
2024-12-08 19:29:17 +0000

    28

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -539,8 +539,8 @@ function roundToSmallestInteger(number) {
  * 5.1  => 6
  * -5.9 => -5
  */
-function roundToLargestInteger(/* number */) {
-  throw new Error('Not implemented');
+function roundToLargestInteger(number) {
+  return Math.ceil(number);
 }
 
 /**
@@ -554,8 +554,8 @@ function roundToLargestInteger(/* number */) {
  * 5.4  => 5
  * -5.5 => -5
  */
-function roundToNearestInteger(/* number */) {
-  throw new Error('Not implemented');
+function roundToNearestInteger(number) {
+  return Math.round(number);
 }
 
 /**
